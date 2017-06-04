1231ea42f0ec7a8ba662b1c24a53c5b753e0a9df
Pinal Bhatt <pinal.bhatt@weightwatchers.com>
2017-06-04 15:00:39 -0400

    commit

--- themes/material/scripts/post_helpers.js
+++ themes/material/scripts/post_helpers.js
@@ -1,9 +1,4 @@
-
-
-hexo.extend.helper.register('twitter_share_link', function(item){
-  const title = item.title;
-  const url = encodeURIComponent(item.permalink);
-  const via = 'PBDesk';
+const getShareData = (item) => {
   const hashTagsArr = [];
   item.categories.forEach(function(category) {
     hashTagsArr.push(category.name);
@@ -12,6 +7,30 @@ hexo.extend.helper.register('twitter_share_link', function(item){
     hashTagsArr.push(tag.name);
   });
   const hashtags = hashTagsArr.join(',');
-  return `https://twitter.com/intent/tweet?url=${url}&text=${title}&via=${via}&hashtags=${hashtags}`;
-  
+  return {
+    title: item.title,
+    url: encodeURIComponent(item.permalink),
+    via: 'PBDesk',
+    hashtags: hashtags
+  }
+};
+
+hexo.extend.helper.register('twitter_share_link', function(item){
+  const d = getShareData(item);
+  return `https://twitter.com/intent/tweet?url=${d.url}&text=${d.title}&via=${d.via}&hashtags=${d.hashtags}`;
+});
+
+hexo.extend.helper.register('facebook_share_link', function(item){
+  const d = getShareData(item);
+  return `https://www.facebook.com/sharer.php?u=${d.url}`;
+});
+
+hexo.extend.helper.register('googleplus_share_link', function(item){
+  const d = getShareData(item);
+  return `https://plus.google.com/share?url=${d.url}`;
+});
+
+hexo.extend.helper.register('linkedin_share_link', function(item){
+  const d = getShareData(item);
+  return `https://www.linkedin.com/shareArticle?url=${d.url}&title=${d.title}`;
 });
